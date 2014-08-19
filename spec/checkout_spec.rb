7ba38134e06d1e3a4e70cd966d886c5dca86b2fe
Meghan Lindsley <student@epicodus.com>
2014-08-19 12:16:49 -0700

    add relation between check out and cart

---
+++ spec/checkout_spec.rb
@@ -0,0 +1,13 @@
+require 'spec_helper'
+
+describe Checkout do
+  it 'will have many carts' do
+    test_customer = Customer.create({name: "JOHN"})
+    test_cashier = Cashier.create({name: "JIMBOB"})
+    test_checkout = Checkout.create ({customer_id: test_customer.id, cashier_id: test_cashier.id})
+    test_product = Product.create({name: 'whatever', price: 56.78})
+    test_cart = Cart.create({product_id: test_product.id, quantity: 5, checkout_id: test_checkout.id})
+    test_cart1 = Cart.create({product_id: test_product.id, quantity: 4, checkout_id: test_checkout.id})
+    expect(test_checkout.carts).to eq [test_cart, test_cart1]
+  end
+end
