46b7233b1e3864a19115622ac9baad599208c00d
Meghan Lindsley <student@epicodus.com>
2014-08-19 15:52:00 -0700

    add function that tells how many customers a cashier as processed.

--- lib/cashier.rb
+++ lib/cashier.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 class Cashier < ActiveRecord::Base
   has_many(:checkouts)
+
+  def total_customers
+    customer_total = 0
+    Checkout.all.each do |checkout|
+      if checkout.cashier_id == self.id
+        customer_total +=1
+      end
+    end
+    customer_total
+  end
 end
+
--- spec/cashier_spec.rb
+++ spec/cashier_spec.rb
@@ -8,4 +8,15 @@ describe Cashier do
     test_checkout2 = Checkout.create({customer_id: test_customer.id, cashier_id: test_cashier.id})
     expect(test_cashier.checkouts).to eq [test_checkout1, test_checkout2]
   end
+
+  it 'will tally customers per cashier' do
+    test_customer = Customer.create({name: "JOHN"})
+    test_customer_two = Customer.create({name: "JON"})
+    test_cashier = Cashier.create({name: "JIMBOB"})
+    test_checkout = Checkout.create({customer_id: test_customer.id, cashier_id: test_cashier.id})
+    test_checkout1 = Checkout.create({customer_id: test_customer_two.id, cashier_id: test_cashier.id})
+    test_product = Product.create({name: 'Dawn', price: 3.99})
+    test_cart1 = Cart.create({product_id: test_product.id, quantity: 5, checkout_id: test_checkout.id})
+    expect(test_cashier.total_customers).to eq 2
+  end
 end
