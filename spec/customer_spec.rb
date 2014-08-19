a383855d95299644d47a819f24b2afab57b4678a
Meghan Lindsley <student@epicodus.com>
2014-08-19 12:11:01 -0700

    add relation between customer and checkout

--- spec/customer_spec.rb
+++ spec/customer_spec.rb
@@ -1,8 +1,11 @@
-# require 'spec_helper'
+require 'spec_helper'
 
-# describe Customer do
-#   it 'will have many checkouts' do
-#     test_customer = Customer.create({name: "JOHN"})
-#     test_cashier = Cashier.create({name: "JIMBOB"})
-#     test_checkout = Checkout.create({customer_id: test_customer.id, cashier_id: test_cashier.id})
-# end
+describe Customer do
+  it 'will have many checkouts' do
+    test_customer = Customer.create({name: "JOHN"})
+    test_cashier = Cashier.create({name: "JIMBOB"})
+    test_checkout1 = Checkout.create({customer_id: test_customer.id, cashier_id: test_cashier.id})
+    test_checkout2 = Checkout.create({customer_id: test_customer.id, cashier_id: test_cashier.id})
+    expect(test_customer.checkouts).to eq [test_checkout1, test_checkout2]
+  end
+end
